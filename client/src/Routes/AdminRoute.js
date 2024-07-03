aa5287d1dad35100e125b4d59b666dc30a66f124
Shashank Kumar <shankjbs571@gmail.com>
2024-07-03 17:01:27 +0530

    create admin dasboard, sidebar

--- client/src/Routes/AdminRoute.js
+++ client/src/Routes/AdminRoute.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import AdminHome from '../admin/AdminHome';
 
 function AdminRoute() {
+  // return (
+  //   const adminRoutes = [
+  //     { path: '/admin', exact: true, component: AdminDashboard },
+  //     { path: '/admin/settings', component: AdminSettings },
+  //     { path: '/admin/users', component: AdminUsers },
+  //     { path: '/admin/login', component: AdminLogin },
+  //   ];
+  // )
   return (
     <div>
-      Admin
+      
+      <Routes>
+        <Route path="/admin" element={<AdminHome />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default AdminRoute
